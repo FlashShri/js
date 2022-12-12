c0dbc13c25f6c5494d537a4cab27f43d89120df2
FlashShri <shrikantdhengle3101@gmail.com>
2022-12-12 22:20:44 +0530

    gg

--- javascriptEX/array.js
+++ javascriptEX/array.js
@@ -2,8 +2,19 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9] ;
 
 let arrLength = arr.length ;
-console.log(arrLength);
-console.log(arr);
+// console.log(arrLength);
+// console.log(arr);
 
-console.log(arr.slice());
-console.log(arr.slice(1, 4));
+// console.log(arr.slice());
+// console.log(arr.slice(1, 4));
+
+// console.log(arr);
+
+//console.log(arr.splice(3 ,6 ));
+//console.log(arr.splice(1 ,0 ,44));
+
+const cars = [ "Tesla", "Tata", "MAhindra", "Hyundai", "BMW"];
+
+cars.splice(0 ,0 , "Bugatti");
+cars.splice(3 ,1 , "Suzuki");
+console.log(cars);
