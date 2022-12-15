853a180eb26565125b521ecb455b5d7db7a9899e
FlashShri <shrikantdhengle3101@gmail.com>
2022-12-15 12:26:08 +0530

    save it

--- javascriptEX/array.js
+++ javascriptEX/array.js
@@ -18,3 +18,37 @@ const cars = [ "Tesla", "Tata", "MAhindra", "Hyundai", "BMW"];
 cars.splice(0 ,0 , "Bugatti");
 cars.splice(3 ,1 , "Suzuki");
 console.log(cars);
+
+cars.push("kk");
+console.log(cars);
+
+// var x ; // declareing the vbarible
+
+// x = 5 ; // assing a vaue to varianle
+
+// //let y = 5 ; // we can assing this way also
+// const z = 10 ; 
+
+// var a = 4 ;
+// var b = 2 ;
+
+// console.log(a*b); // here a*b will be 8
+
+// {
+//   var a = 6 ;
+//   console.log(a*b); // here a*b will be 12
+// }
+
+// console.log(a*b); // here a*b will be 12
+
+let a = 4 ;
+let b = 2 ;
+
+console.log(a*b); // here a*b will be 8
+
+{
+  let a = 6 ;  // this redeclared varible has only block scope
+  console.log(a*b); // here a*b will be 12
+}
+
+console.log(a*b); // here a*b will be 8
