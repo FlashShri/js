71bbd5860da2029b8dae77dfbb808fbf983afeb6
FlashShri <shrikantdhengle3101@gmail.com>
2022-12-16 20:22:48 +0530

    add

--- demo/pageScript.js
+++ demo/pageScript.js
@@ -1,9 +1,46 @@
 
 
 
-
+// This is JavaSCript File
 
 function displayAlert(){
 
-    alert("Hi welcome to Terras-1");
+    alert( 12+2+2+2+"5" + 10);
 }
+function displayPromt(){
+    let demo = "Elon Musk" ;
+    let userName = prompt("Enter your Username:" , demo );
+
+    console.log(userName);
+}
+function displayConfir() {
+    confirm("Do you accept terms of this site?");
+}
+
+// let a = "333" ;
+// a = 333 ;
+// console.log(typeof a) ;
+// console.log(typeof NaN) ;
+// console.log(10 + "5") ;
+// console.log(true * 10);
+// console.log(false * 10);
+// console.log("five" % 5);
+
+let af = null ; // we can set null value
+af = Number(af);
+console.log(typeof af);
+
+let str = "   36 45";
+str = Number(str);
+console.log(typeof str);
+console.log(str);
+
+// let value = "1";
+// value = Boolean(value);
+// console.log(typeof value);
+// console.log(value);
+
+let value = "   STRING 2323" //space
+value = Boolean(value);
+console.log(typeof value);
+console.log(value);
